560d8fd5b839fadd9f62d90616700903b5c39441
IT19395906 <IT19395906@my.sliit.lk>
2025-08-21 01:20:46 +0530

    env file added

---
+++ src/app/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  //dev apiUrl: 'http://00.00.00.00:0000/',
+
+  // QA apiUrl: 'http://00.00.00.00:0000/',
+
+  //Prod apiUrl: 'https://sample.lk',
+
+  //UAT
+  apiUrl: 'https://sample.lk',
+};
